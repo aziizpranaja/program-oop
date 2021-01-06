55ae70545e24cfc1bf3683ac9570ae770c933232
aziizpranaja <56227588+aziizpranaja@users.noreply.github.com>
2021-01-06 20:54:40 +0700

    Add files via upload

---
+++ restoran/src/restoran/Restoran.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restoran;
+
+/**
+ *
+ * @author AZIIZ
+ */
+public class Restoran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new restoran_form().show();
+    }
+    
+}
